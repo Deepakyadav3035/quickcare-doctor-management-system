34749fcbf1f664af025828389bee6955eacf1dff
Deepak Kumar Yadav <deepakyadav169169@gmail.com>
2026-08-01 23:58:51 +0530

    QC-17 Implemented DoctorController CRUD APIs

--- src/main/java/com/quickcare/repository/DoctorRepository.java
+++ src/main/java/com/quickcare/repository/DoctorRepository.java
@@ -4,6 +4,13 @@ import com.quickcare.entity.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface DoctorRepository extends JpaRepository<Doctor,Long> {
+    List<Doctor> email(String email);
+    Optional<Doctor>findDoctorByEmail(String email);
+
+    Optional<Doctor> findDoctorById(Long id);
 }
