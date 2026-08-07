acd7d25a22c4222616bf1624a3b759ae3509e72b
Deepak Kumar Yadav <deepakyadav169169@gmail.com>
2026-08-07 12:19:53 +0530

    QC-18 : Implemented global exception handling

---
+++ src/main/java/com/quickcare/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.quickcare.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+}
